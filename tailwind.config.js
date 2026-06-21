33d6090e44c064b9ca7e9ab4dff4c6bc72175757
문명국 <civilizednation@gmail.com>
2026-06-21 17:46:23 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Nunito", "Noto Sans KR", "sans-serif"]
+      }
+    }
+  },
+  plugins: []
+};
